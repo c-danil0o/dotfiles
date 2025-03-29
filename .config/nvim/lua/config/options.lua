e4b394c00a87c1fc8e3fbd6234cb281e8102f4df
Danilo <danilo.cvijetic10@protonmail.ch>
2025-03-29 14:24:15 +0100

    some rule fixes and nvim update

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,17 +4,16 @@
 
 -- vim.opt.shiftwidth = 4 -- Size of an indent
 if vim.g.neovide then
-  vim.opt.linespace = 1
   vim.g.neovide_floating_blur_amount_x = 0.0
   vim.g.neovide_floating_blur_amount_y = 0.0
   vim.g.neovide_floating_shadow = false
   vim.g.neovide_scale_factor = 1.0
   vim.g.neovide_hide_mouse_when_typing = false
+  vim.g.neovide_floating_corner_radius = 0.5
+  vim.g.neovide_refresh_rate = 90
   vim.g.neovide_cursor_animation_length = 0.07
   vim.g.neovide_cursor_trail_size = 0.5
   vim.g.neovide_cursor_animate_in_insert_mode = false
-  vim.g.neovide_text_gamma = 0.2
-  vim.g.neovide_text_contrast = 0.8
   local change_scale_factor = function(delta)
     vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
   end
--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -8,6 +8,43 @@ return {
     ---@type lspconfig.options
     inlay_hints = { enabled = false },
     servers = {
+
+      clangd = {
+        keys = {
+          { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C/C++)" },
+        },
+        root_dir = function(fname)
+          return require("lspconfig.util").root_pattern(
+            "Makefile",
+            "configure.ac",
+            "configure.in",
+            "config.h.in",
+            "meson.build",
+            "meson_options.txt",
+            "build.ninja"
+          )(fname) or require("lspconfig.util").root_pattern("compile_commands.json", "compile_flags.txt")(
+            fname
+          ) or require("lspconfig.util").find_git_ancestor(fname)
+        end,
+        capabilities = {
+          offsetEncoding = { "utf-16" },
+        },
+        cmd = {
+          "clangd",
+          "--background-index",
+          "--clang-tidy",
+          "--header-insertion=iwyu",
+          "--completion-style=detailed",
+          "--function-arg-placeholders",
+          "--fallback-style=llvm",
+          "--header-insertion=never",
+        },
+        init_options = {
+          usePlaceholders = true,
+          completeUnimported = true,
+          clangdFileStatus = true,
+        },
+      },
       gopls = {
         settings = {
           gopls = {
